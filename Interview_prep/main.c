906641f1c94589a64d2ccf92ce2881c7b930887d
raghus100 <raghus100@gmail.com>
2019-10-21 18:43:08 -0700

    Initial Commit

---
+++ Interview_prep/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Interview_prep
+//
+//  Created by ragsekar on 10/21/19.
+//  Copyright © 2019 ragsekar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) 
+{
+    printf("Hello World\n");
+    return 0;
+}
---
+++ Interview_prep/main.h
@@ -0,0 +1,15 @@
+//
+//  main.h
+//  Interview_prep
+//
+//  Created by ragsekar on 10/21/19.
+//  Copyright © 2019 ragsekar. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif /* main_h */
